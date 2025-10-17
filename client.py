8b6e7ecb278779765180f7fe1898ac1cf735f2f7
aneesh.damle <aneesh.damle@protonmail.com>
2025-10-17 15:47:27 +0530

    exception handling

--- client.py
+++ client.py
@@ -1,17 +1,26 @@
 # client using asyncio websockets
 import asyncio
 import websockets
+import sys
 
 
 async def hello():
     async with websockets.connect("ws://localhost:8080") as websocket:
         while True:
-            message = input("Enter message: ")
+            try:
+                message = input("Enter message: ")
+            except EOFError:
+                print("User entered Ctrl-D while typing")
+                sys.exit()
             await websocket.send(message)
             response = await websocket.recv()
             print("Response: ", message)
 
 
 if __name__ == "__main__":
-    asyncio.run(hello())
-
+    try:
+        print("Starting client (exit on Ctrl-C)...")
+        asyncio.run(hello())
+    except KeyboardInterrupt:
+        print("\nExiting client on Ctrl-C")
+        sys.exit()
--- server.py
+++ server.py
@@ -1,19 +1,38 @@
 # echo server with asyncio websockets
 import asyncio
 import websockets
+import sys
 
 
-async def echo(websocket):
-    async for message in websocket:
-        print(message)
-        await websocket.send(message)
+members = set()
+
+async def chat(websocket):
+    members.add(websocket)
+    try:
+        async for message in websocket:
+            print(message)
+            # broadcast message to all members
+            print("Current members: ", members)
+            for member in members:
+                if member != websocket:
+                    await member.send(message)
+    except websockets.exceptions.ConnectionClosed:
+        pass
+    finally:
+        print("Removing member: ", websocket)
+        members.remove(websocket)
 
 
 async def main():
-    server = await websockets.serve(echo, "localhost", 8080)
+    server = await websockets.serve(chat, "localhost", 8080)
     await server.serve_forever()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        print("Starting server (exit on Ctrl-C)...")
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nExiting the server on Ctrl-C")
+        sys.exit()
 
